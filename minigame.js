016eedd5a4e9d9db4170037f17da0a1ff0cdbc2b
KiMiRe <milanoinrock01@gmail.com>
2026-03-23 08:04:38 +0700

    Update dialogue and timing in minigame.js

--- minigame.js
+++ minigame.js
@@ -9,7 +9,7 @@ const botSay = (data) => {
         "Jadi, siapa namamu?",
         `wahh halo ${data?.nama}☺️! Itu nama yang keren. apa kamu senang jika ada orang yang memanggilmu dengan nama itu?`,
         `Hehehe... kamu tahu? aku selalu iri mengetahui manusia bisa bebas berekspresi, sedangkan aku hanya program yang dirangcang oleh mereka. apa kamu tahu apa yang bisa menghiburku?`,
-        `aku rasa ${data?.nyocot} tapi... ayo kita bermain game. bukankah game opsi yang terbaik untuk meninggalkan pekerjaan kita?`,
+        `aku rasa menggambar menyenangkan tapi... ayo kita bermain game. bukankah game opsi yang terbaik untuk meninggalkan pekerjaan kita?`,
         `Ah bercanda, pekerjaan merupakan hal wajib untuk kita yang sedang di usia produktif, jadi gimana kita akan bermain game? Apakah di usiamu saat ini masih layak untuk main game? Berapa usiamu sekarang?`,
         `aku rasa usia ${data?.usia} tidak begitu buruk. 😉`,
     ]
@@ -25,30 +25,50 @@ function botStart() {
     if (init === 1) {
         botDelay({ nama: jawaban.value })
             setTimeout(() => {
-                jawaban.placeholder = "iya/tidak";
-            }, [1250])
+                containerJawaban.style.display = 'none';
+                btnKirim.innerHTML = 'ya, aku suka mendengar namaku.';
+                userData.push(jawaban.value)
+                jawaban.value = "ya, aku suka mendengar namaku."
+                btnKirim.style.fontSize = ".6vh"
+            }, [1750])
     } else if (init === 2) {
         botDelay({ canggung: jawaban.value })
             setTimeout(() => {
+                containerJawaban.style.display = 'block';
                 jawaban.placeholder = "tulis saranmu di sini!";
-            }, [1250])
+                userData.push(jawaban.value)
+                jawaban.value = "Mungkin kegiatan menyenangkan seperti, menggambar."
+                btnKirim.innerHTML = 'send';
+                btnKirim.style.fontSize = "1.4vh"
+            }, [1750])
     } else if (init === 3) {
         botDelay({ nyocot: jawaban.value })
             setTimeout(() => {
-                jawaban.value = "tunggu, apa maksudmu itu?"
-            }, [1250])
+                containerJawaban.style.display = 'none';
+                btnKirim.innerHTML = 'Tunggu, apa maksudmu itu!?';
+                userData.push(jawaban.value)
+                jawaban.value = "Tunggu, apa maksudmu itu!?"
+                btnKirim.style.fontSize = ".6vh"
+            }, [1750])
     } else if (init === 4) {
         botDelay({ gitulah: jawaban.value })
             setTimeout(() => {
+                containerJawaban.style.display = 'block';
                 jawaban.placeholder = "tulis usiamu di sini!";
-            }, [1250])
+                btnKirim.innerHTML = 'send';
+                userData.push(jawaban.value)
+                jawaban.value = ""
+                btnKirim.style.fontSize = "1.4vh"
+            }, [1750])
     } else if (init === 5) {
         botDelay({ usia: jawaban.value })
             setTimeout(() => {
-                jawaban.placeholder = "jawab dia di sini!";
+                containerJawaban.style.display = 'none';
+                btnKirim.innerHTML = 'Okie dokie...';
                 userData.push(jawaban.value)
                 jawaban.value = "Okie dokie..."
-            }, [1250])
+                btnKirim.style.fontSize = ".8vh"
+            }, [1750])
     } else if (init === 6) {
         finishing({ nya: jawaban.value })
     } else {
@@ -60,7 +80,7 @@ function botDelay(jawabanUser) {
     console.log({ userData: userData })
     setTimeout(() => {
         pertanyaan.innerHTML = botSay(jawabanUser)[init]
-    }, [1250])
+    }, [1750])
     userData.push(jawaban.value)
     jawaban.value = ""
 }
