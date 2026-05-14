2abd308e37bfa8402b7ad9c9540cffd2d9b07148
KiMiRe <milanoinrock01@gmail.com>
2026-05-14 23:48:36 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -14,68 +14,94 @@ document.getElementById("a-content-rencana").addEventListener("click", function(
     });
 });
 
+
+downloadDiManaAkuMelangkah.addEventListener("click", function() {
+    setTimeout(function() {
+        unduhDiManaAkuMelangkah.style.display = 'block';
+    }, 450);
+    batalUnduhDiManaAkuMelangkah.addEventListener('click', function() {
+        unduhDiManaAkuMelangkah.style.display = 'none';
+    });
+});
+
+itchio.addEventListener("click", function() {
+    window.location.href = 'https://kimirenai.itch.io/di-mana-aku-melangkah';
+});
+
+infoDiManaAkuMelangkah.addEventListener("click", function() {
+    setTimeout(function() {
+        alert('Akhhh... What a procrastination.')
+    }, 450);
+});
+
+kontributorDiManaAkuMelangkah.addEventListener("click", function() {
+    setTimeout(function() {
+        window.location.href = 'kontributorDiManaAkuMelangkah.html';
+    }, 450);
+});
+
 download.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'https://kimirenai.itch.io/who-siapa-nama-mereka';
-    }, 750);
+    }, 450);
 });
 
 infowho.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'infowho.html';
-    }, 750);
+    }, 450);
 });
 
 info.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'info.html';
-    }, 750);
+    }, 450);
 });
 
 karakter.addEventListener("click", function() {
     setTimeout(function() {
         alert('Akhhh... What a procrastination.')
-    }, 750);
+    }, 450);
 });
 
 kontributorwho.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'kontributorwho.html';
-    }, 750);
+    }, 450);
 });
 
 kontributorrefusal.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'kontributorrefusal.html';
-    }, 750);
+    }, 450);
 });
 
 youtube.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'https://youtube.com/@milan_oy?si=IG19uNKr7bRHX3Cy';
-    }, 750);
+    }, 450);
 });
 
 facebook.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'https://www.facebook.com/share/1EmryehnEB/';
-    }, 750);
+    }, 450);
 });
 
 tiktok.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'http://tiktok.com/@kimirenaissance';
-    }, 750);
+    }, 450);
 });
 
 sociabuzz.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'https://sociabuzz.com/milan_o391/tribe';
-    }, 750);
+    }, 450);
 });
 
 trakteer.addEventListener("click", function() {
     setTimeout(function() {
         window.location.href = 'https://trakteer.id/kimi_renaissance';
-    }, 750);
+    }, 450);
 });
